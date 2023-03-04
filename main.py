6906a29063b36a3ab072fcb6262f284d88b04633
Elvee Bolt <boltelvee@gmail.com>
2023-03-04 11:55:29 +0200

    Edited homework conception

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def runtime_function(func):
     :param func: object of func
     :return:
     """
+
     def wrapper(*args, **kwargs):
         time_start = datetime.now()
         func(*args, **kwargs)
@@ -27,42 +28,51 @@ def genarate_data() -> str:
     return string.ascii_lowercase * 100000000
 
 
-@runtime_function
-def write_file(data: str):
+def write_file(data: str, title: str):
     """
     Write generated data to file
     :param data: str of data
     :return:
     """
 
-    with open(file='file.txt', mode='w', encoding='utf-8') as file:
+    with open(file=f'file_{title}.txt', mode='w', encoding='utf-8') as file:
         file.write(data)
 
 
-def main():
-    data = genarate_data()
+@runtime_function
+def task_main(data):
+    print('\nTime work with - Main')
+    write_file(data=data, title='main')
 
-    # One task
-    print('Time work with one task')
-    write_file(data)
 
-    # Two task Threading
+@runtime_function
+def task_thread(data: str):
     print('\nTime work with two task - Threading')
-    thread1 = Thread(target=write_file, args=(data,))
-    thread2 = Thread(target=write_file, args=(data,))
+    thread1 = Thread(target=write_file, kwargs={'data': data, 'title': 'thread1'})
+    thread2 = Thread(target=write_file, kwargs={'data': data, 'title': 'thread2'})
     thread1.start()
     thread2.start()
     thread1.join()
     thread2.join()
 
-    # Two task Process
+
+@runtime_function
+def task_process(data: str):
     print('\nTime work with two task - Process')
-    process1 = Process(target=write_file, args=(data,))
-    process2 = Process(target=write_file, args=(data,))
+    process1 = Process(target=write_file, kwargs={'data': data, 'title': 'process1'})
+    process2 = Process(target=write_file, kwargs={'data': data, 'title': 'process2'})
     process1.start()
     process2.start()
     process1.join()
     process2.join()
+
+
+def main():
+    data = genarate_data()
+
+    task_main(data)
+    task_thread(data)
+    task_process(data)
 
 
 if __name__ == '__main__':
